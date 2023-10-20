0ee5481d054e570a52a7e65fcd5cadc4742505aa
JacobChartier <55593549+JacobChartier@users.noreply.github.com>
2023-10-19 22:51:16 -0400

    2023.10.19b - New inputs
    Additions :
     - Added an input to shoot a projectile
     - Added an input to pause de game
    
    Modifications :
     - Modified de name of Mouvement.cs to PlayerMovement.cs

--- RythmicSlash/Assets/Scripts/Player/InputManager.cs
+++ RythmicSlash/Assets/Scripts/Player/InputManager.cs
@@ -7,11 +7,15 @@ public class InputManager : MonoBehaviour
     [SerializeField] private KeyCode moveLeftKey = KeyCode.A;
     [SerializeField] private KeyCode moveRightKey = KeyCode.D;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode shootKey = KeyCode.F;
+    [SerializeField] private KeyCode pauseGameKey = KeyCode.Escape;
 
     [Header("Events")]
     [SerializeField][Space] private UnityEvent OnMoveLeftKeyPressed;
     [SerializeField][Space] private UnityEvent OnMoveRightKeyPressed;
     [SerializeField][Space] private UnityEvent OnJumpKeyPressed;
+    [SerializeField][Space] private UnityEvent OnShootKeyPressed;
+    [SerializeField][Space] private UnityEvent OnPauseGameKeyPressed;
 
     private void Update()
     {
@@ -28,6 +32,19 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown(jumpKey))
         {
             OnJumpKeyPressed?.Invoke();
+            Debug.Log($"A key has been pressed: <color=#FFFF00>{jumpKey}</color>");
+        }
+
+        if (Input.GetKeyDown(shootKey))
+        {
+            OnShootKeyPressed?.Invoke();
+            Debug.Log($"A key has been pressed: <color=#FFFF00>{shootKey}</color>");
+        }
+
+        if (Input.GetKeyDown(pauseGameKey))
+        {
+            OnPauseGameKeyPressed?.Invoke();
+            Debug.Log($"A key has been pressed: <color=#FFFF00>{pauseGameKey}</color>");
         }
     }
 }
