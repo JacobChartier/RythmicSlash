bf0edf1a2563456aa2121b09513cdae65690a25a
JacobChartier <55593549+JacobChartier@users.noreply.github.com>
2023-11-07 10:50:34 -0500

    2023.11.07b - Health system

--- RythmicSlash/Assets/Scripts/Entities/Health.cs
+++ RythmicSlash/Assets/Scripts/Entities/Health.cs
@@ -1,20 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private int MAX_HEALTH = 10;
-    [SerializeField] private int currentHealth { get; set; }
+    [SerializeField] public int currentHealth { get; private set; }
+
+    [SerializeField] private UnityEvent OnGainHealth;
+    [SerializeField] private UnityEvent OnLoseHealth;
+    [SerializeField] private UnityEvent OnDeath;
 
     void Awake()
     {
         currentHealth = MAX_HEALTH;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GainHealth(int amount)
     {
-        
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, MAX_HEALTH);
+
+        OnGainHealth?.Invoke();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, MAX_HEALTH);
+
+        OnLoseHealth?.Invoke();
+
+        if (currentHealth < 1)
+            OnDeath?.Invoke();
     }
 }
